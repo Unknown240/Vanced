ae8a4d904a7f63bac6d7e9f5d3cbd9afbc77c222
Calvin Firlansyah <muhammadlinggar9@gmail.com>
2022-08-22 05:23:33 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,30 @@
+clear
+echo " ReVanced Builder & Patch "
+
+echo " 1. Start Vanced "
+echo " 2. Update Vanced "
+echo " 3. Reinstall Vanced "
+echo -e 
+echo " e. Exit "
+
+echo " Choice :"
+
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ] 
+then
+curl -sLo revanced-builder.sh https://raw.githubusercontent.com/reisxd/revanced-builder/main/android-interface.sh
+chmod +x revanced-builder.sh
+mv revanced-builder.sh .revanced-builder.sh
+bash .revanced-builder.sh
+fi
+
+if [ $mrrm = 2 ] || [ $mrrm = 2 ]
+then 
+bash .revanced-builder.sh update
+bash run.sh
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+bash .revanced-builder.sh reinstall
+fi
